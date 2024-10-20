5ebc45339a24f1354133d7683dd5c916e1d7c260
itAshmit <166281292+itAshmit@users.noreply.github.com>
2024-10-20 17:42:07 +0530

    mademy first component named Greet, yayyyyy!

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Greet from "./component/greet"
+const App = () =>{
+  return(
+  <div>
+    <Greet/>  
+  </div>
+  );
+}
+
+export default App;
---
+++ src/component/greet.jsx
@@ -0,0 +1,4 @@
+function Greet(){
+    return <h1>Greetings from me</h1>
+}
+export default Greet;
